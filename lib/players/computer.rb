d7db107f14bde264a6aa17d4c7850688d7965f12
Lindsey Trainor <ltrainor38@gmail.com>
2018-03-10 02:48:25 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,26 +3,22 @@ class Computer < Players
   def move(board)
 
       index = 5
-  
+
     good_combo = wins.detect do |combo|
       combo = combo.each {|space| space += 1}
-      a = board.taken(combo(1)) && board.taken(combo(2)) 
-      b = board.taken(combo(1)) && board.taken(combo(3)) 
+      a = board.taken(combo(1)) && board.taken(combo(2))
+      b = board.taken(combo(1)) && board.taken(combo(3))
       c = board.taken(combo(2)) && board.taken(combo(3))
-      a || b || c 
-      if a 
+      a || b || c
+      if a
         index = combo[3]
-      elsif b 
+      elsif b
         index = combo[2]
-      else 
+      else
         index = combo[1]
-      end 
-    end 
+      end
+    end
 
     index = 1 + rand(9)
     index.to_s
-end 
-    
-    
-    
-
+end
